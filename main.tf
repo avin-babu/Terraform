bb4ba78cbe84f8811d8f944bd8c0d98d829944aa
avin-babu <avinbabuk12@gmail.com>
2025-11-23 15:01:42 +0530

    add jenkins file and changes in main.tf file

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-s3-bucket-31102025 "
+    bucket        = "terraform-s3-bucket-31102025"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     use_lockfile = true
